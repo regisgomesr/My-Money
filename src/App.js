3bb92c397d192a76f314e8798307fcdb3219e919
Regis Ribeiro <regisgomesr@gmail.com>
2019-09-10 16:44:48 -0300

    Organizando arquivos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+
+/*axios
+.get('https://mymoney-regis.firebaseio.com/name.json')
+.then(res => {
+  console.log(res)
+})
+
+axios
+.post('https://mymoney-regis.firebaseio.com/name.json', {
+  outro: 'Theo'
+})
+.then(res => {
+  console.log(res)
+})*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Money</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
