200369e345b62e59342e9b0ae172469ceacd354c
Regis Ribeiro <regisgomesr@gmail.com>
2019-09-10 18:25:25 -0300

    Consumindo dados da API Rest Firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect, useReducer} from 'react'
 import axios from 'axios'
 
 /*axios
@@ -15,10 +15,49 @@ axios
   console.log(res)
 })*/
 
+const url = 'https://mymoney-regis.firebaseio.com/movimentacoes/2019-08.json'
+
+// funcao pura = vantagem que é melhor para testar
+const reducer = (state, action) => {
+  console.log('state', state, 'action', action)
+  if(action.type === 'REQUEST') {
+    return {
+      ...state,
+      loading: true
+    }
+  }
+
+  if(action.type === 'SUCCESS') {
+    return {
+      ...state,
+      loading: false,
+      data: action.data
+    }
+  }
+
+  return state
+}
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, {
+    loading: true,
+    data: {}
+  })
+  
+  useEffect(() => {
+    dispatch({ type: 'REQUEST' })
+    axios
+    .get(url)
+    .then(res => {
+     dispatch({ type: 'SUCCESS', data: res.data })
+    })
+  }, [])
+  
   return (
     <div>
       <h1>My Money</h1>
+      { JSON.stringify(data) }
+      { data.loading && <p>Loading...</p> }
     </div>
   )
 }
