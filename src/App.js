8b41b585ffe9182502cd13e66d753dd6d562caf8
Regis Ribeiro <regisgomesr@gmail.com>
2019-09-10 19:25:42 -0300

    Incluindo arquivo useGet e modificando src/App - utilizando hooks personalizados

--- src/App.js
+++ src/App.js
@@ -1,63 +1,19 @@
-import React, { useState, useEffect, useReducer} from 'react'
-import axios from 'axios'
+import React from 'react'
+import useGet from './useGet'
 
-/*axios
-.get('https://mymoney-regis.firebaseio.com/name.json')
-.then(res => {
-  console.log(res)
-})
-
-axios
-.post('https://mymoney-regis.firebaseio.com/name.json', {
-  outro: 'Theo'
-})
-.then(res => {
-  console.log(res)
-})*/
 
 const url = 'https://mymoney-regis.firebaseio.com/movimentacoes/2019-08.json'
 
-// funcao pura = vantagem que é melhor para testar
-const reducer = (state, action) => {
-  console.log('state', state, 'action', action)
-  if(action.type === 'REQUEST') {
-    return {
-      ...state,
-      loading: true
-    }
-  }
-
-  if(action.type === 'SUCCESS') {
-    return {
-      ...state,
-      loading: false,
-      data: action.data
-    }
-  }
-
-  return state
-}
-
 function App() {
-  const [data, dispatch] = useReducer(reducer, {
-    loading: true,
-    data: {}
-  })
-  
-  useEffect(() => {
-    dispatch({ type: 'REQUEST' })
-    axios
-    .get(url)
-    .then(res => {
-     dispatch({ type: 'SUCCESS', data: res.data })
-    })
-  }, [])
-  
+  const data = useGet(url)
+  const data2 = useGet('http://httpbin.org/ip')
+
   return (
     <div>
       <h1>My Money</h1>
       { JSON.stringify(data) }
       { data.loading && <p>Loading...</p> }
+      <pre>{ JSON.stringify(data2) }</pre>
     </div>
   )
 }
