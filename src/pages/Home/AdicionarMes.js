0137a1a0f070685724b479dc33355cea0adc08c9
Regis Ribeiro <regisgomesr@gmail.com>
2019-09-21 13:55:36 -0300

    Criando um novo mes / Hook useRef

--- src/pages/Home/AdicionarMes.js
+++ src/pages/Home/AdicionarMes.js
@@ -1,20 +1,52 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import { Redirect } from 'react-router-dom'
+
+const minAno = 2019
+const maxAno = 2022
 
 const AdicionarMes = () => {
+
+    const refAno = useRef()
+    const refMes = useRef()
+    const [redir, setRedir] = useState('')
+
+    const anos = []
+    const meses = []
+
+    for(let i = minAno; i<= maxAno; i++) {
+        anos.push(i)
+    }
+
+    for(let i = 1; i<= 12; i++) {
+        meses.push(i)
+    }
+
+    const zeroPad = num => {
+        if(num < 10) {
+            return '0' + num
+        }
+        return num
+    }
+    
+    const verMes = () => {
+        setRedir(refAno.current.value + '-' + refMes.current.value)
+    }
+    if(redir !== ''){
+        return <Redirect to={'/movimentacoes/' + redir} />
+    }
+
     return(
         <React.Fragment>
         <h2>Adicionar mês</h2>
-            <select>
-                <option value='2019'>2019</option>
-                <option value='2020'>2020</option>
+            <select ref={refAno}>
+                { anos.map(ano => <option key={ano} value={ano}>{ano}</option>) }       
             </select>
 
-            <select>
-                <option value='01'>01</option>
-                <option value='02'>02</option>
+            <select ref={refMes}>
+                { meses.map(zeroPad).map(mes => <option key={mes} value={mes}>{mes}</option>) }
             </select>
 
-            <button>Adicionar mês</button>
+            <button onClick={verMes}>Adicionar mês</button>
         </React.Fragment>    
     )
 }
